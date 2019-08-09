ee2b24f81ee0652fb8121f5a70c20ca37de699ba
Meenakshi Venkat <meenak.venkat@gmail.com>
2019-08-09 10:16:42 +0200

    Modify index.js to fix console fn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const express = require('express')
 const app = express()
 const port = 5000 || process.env.port
 
-app.listen(port, console.log(`Listening on port ${port}..`))
+app.listen(port, () => console.log(`Listening on port ${port}..`))
