b0179e158c287f03d95b3c954adcff33435c0178
Meenakshi Venkat <meenak.venkat@gmail.com>
2019-08-09 10:27:49 +0200

    Modify port order

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
 const app = express()
-const port = 5000 || process.env.port
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Listening on port ${port}..`))
